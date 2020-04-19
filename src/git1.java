052fbda698129f41041bcc13b1bb33e0229dad6b
valli <vpannala@gmail.com>
2020-04-19 15:29:58 -0700

    second commit

--- src/git1.java
+++ src/git1.java
@@ -16,6 +16,23 @@ public class git1 {
 		
 		//Git is intialized and ready to push the code into the repository.
 		//Local and branch master
+		//it is given by git init
+		//git add * is for adding the files in the repository
+		//create a new repository
+		// user name and password should be given for creating git hub account
+		//user name avp1234
+		//password Srikar1234!@#$
+		// 
+		
+		//Set up the cloud repository using git remote -v
+		//copy the repostorr git remote -v  https:// https://github.com/avp1234/Git-Demo.git
+		//give the git config --global user.name "avp1234"
+		// git config --global user.email "vpannala@gmail.com"
+		//give git commit -m "first commit"
+		//give git push -u origin master
+		//the above command pushes the code in to the cloud repository
+		//git branch
+		
 		
 		
 		
