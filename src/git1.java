9b8eb0d5c518f7326897e60e3086a7f5ab0c6ece
valli <vpannala@gmail.com>
2020-04-19 11:44:07 -0700

    first commit

---
+++ src/git1.java
@@ -0,0 +1,26 @@
+
+public class git1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" I am learning git today...");
+		
+		
+		//Create a git account  https://github.com/settings/emails.
+		
+		//create a new git hub account
+		// create new repository
+		//How to push code into the repository
+		//Git intialize the project
+		
+		//Git is intialized and ready to push the code into the repository.
+		//Local and branch master
+		
+		
+		
+		
+
+	}
+
+}
